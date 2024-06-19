0dda8904a6fc1762381374cc5837ee49e9f6a16f
RaulECT <eduardoct120@gmail.com>
2024-06-19 16:22:22 -0600

    @feat: cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Layout, Typography } from 'antd';
 import Header from './components/Header';
 import NotificationsSection from './components/NotificationsSection';
